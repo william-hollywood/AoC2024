74e9e60bf7c851053825b8ec726208381a525b30
William Hollywood <will@oceanplait.com>
2024-12-07 08:52:12 +1300

    Unit tests implemented for day2 lib functions

--- day2/lib.s
+++ day2/lib.s
@@ -5,6 +5,8 @@
 # read report until newline or null terminator
 # a0 - buffer pos
 # a1 - output pos
+# Returns
+# a0 - list len
 .global parse_report
 parse_report:
 	addi sp, sp, -16
@@ -17,6 +19,7 @@ parse_report:
 
 # is_only_inc_or_dec
 # a0 - report array pos
+# a1 - report array len
 # Returns
 # a0 - !0 if the report only increments or decrements
 .global is_only_inc_or_dec
@@ -30,8 +33,9 @@ is_only_inc_or_dec:
 
 # distance_check
 # a0 - report array pos
-# a1 - min distance
-# a2 - max distance
+# a1 - report array len
+# a2 - min distance
+# a3 - max distance
 # Returns
 # a0 - !0 if the report values are least a1 apart and at most a2 part
 .global distance_check
@@ -48,6 +52,9 @@ distance_check:
 # - numbers only increase or decrease (only inc or dec func)
 # - consecutive numbers are at least 1 apart and at most 3 (disance check func)
 # a0 - report array pos
+# a1 - report array len
+# a2 - min distance
+# a3 - max distance
 # Returns
 # a0 - !0 is the report is safe
 .global is_report_safe
@@ -62,6 +69,8 @@ is_report_safe:
 # count_reports
 # count how many reports are safe
 # a0 - buffer pos
+# a1 - min distance
+# a2 - max distance
 # Returns
 # a0 - number of safe reports
 .global count_reports
--- day2/test.s
+++ day2/test.s
@@ -1,14 +1,228 @@
-.equ PUTS_LOC, 0x86000000
-.equ UART_BASE, 0x10000000
-.equ STACK_POS, 0x88000000
+.equ ARRAY_LOC, 0x86000000
 
 .section .data
-test1name: .string ""
-test1data: .string ""
+parse_report1name: .string "parse_report - correctly parses and returns len:\n"
+parse_report1data: .string "7 9 10 11"
+parse_report1check1: .string "\t[0] =  7: "
+parse_report1check2: .string "\t[1] =  9: "
+parse_report1check3: .string "\t[2] = 10: "
+parse_report1check4: .string "\t[3] = 11: "
+parse_report1check5: .string "\tlen = 16: "
+is_only_inc_or_dec1name: .string "is_only_inc_or_dec - all incrementing: "
+is_only_inc_or_dec2name: .string "is_only_inc_or_dec - all decrementing: "
+is_only_inc_or_dec3name: .string "is_only_inc_or_dec - not all same: "
+distance_check1name: .string "distance_check - all close enough: "
+distance_check2name: .string "distance_check - too far apart: "
+distance_check3name: .string "distance_check - too close together: "
+is_report_safe1name: .string "is_report_safe - safe: "
+is_report_safe2name: .string "is_report_safe - is_only_inc_or_dec fails: "
+is_report_safe3name: .string "is_report_safe - distance_check fails: "
+count_reports1name: .string "count_reports - counts corect number: "
+count_reports1data: .string "1 2 3 4 5\n5 1 4 3 2\n5 4 3 2 1"
 
 # Program main
 .section .text
 	call start
+	addi sp, sp, -16
+
+# TEST: parse_report
+	la a0, parse_report1name
+	call print
+	la a0, parse_report1data
+	li a1, ARRAY_LOC
+	call parse_report
+	sw a1, 0(sp)
+
+	la a0, parse_report1check1
+	call print
+	li a0, 7
+	li t1, ARRAY_LOC
+	lw a1, 0(t1)
+	call test_eq
+
+	la a0, parse_report1check2
+	call print
+	li a0, 9
+	li t1, ARRAY_LOC
+	lw a1, 4(t1)
+	call test_eq
+
+	la a0, parse_report1check3
+	call print
+	li a0, 10
+	li t1, ARRAY_LOC
+	lw a1, 8(t1)
+	call test_eq
+
+	la a0, parse_report1check4
+	call print
+	li a0, 11
+	li t1, ARRAY_LOC
+	lw a1, 12(t1)
+	call test_eq
+
+	la a0, parse_report1check5
+	call print
+	li a0, 16
+	lw a1, 0(sp)
+	call test_eq
+
+# TEST: is_only_inc_or_dec - all inc
+	la a0, is_only_inc_or_dec1name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 7
+	sw t0, 0(a0)
+	li t0, 8
+	sw t0, 4(a0)
+	li t0, 9
+	sw t0, 8(a0)
+	li a1, 12
+	call distance_check
+	li a1, 1
+	call test_eq
+
+# TEST: is_only_inc_or_dec - all dec
+	la a0, is_only_inc_or_dec2name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 9
+	sw t0, 0(a0)
+	li t0, 8
+	sw t0, 4(a0)
+	li t0, 7
+	sw t0, 8(a0)
+	li a1, 12
+	call distance_check
+	li a1, 1
+	call test_eq
+
+# TEST: is_only_inc_or_dec - not all same
+	la a0, is_only_inc_or_dec3name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 7
+	sw t0, 0(a0)
+	li t0, 8
+	sw t0, 4(a0)
+	li t0, 7
+	sw t0, 8(a0)
+	li a1, 12
+	call distance_check
+	li a1, 1
+	call test_eq
+
+# TEST: distance_check - all close enough
+	la a0, distance_check1name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 1
+	sw t0, 0(a0)
+	li t0, 2
+	sw t0, 4(a0)
+	li t0, 5
+	sw t0, 8(a0)
+	li a1, 12
+	li a2, 1
+	li a3, 3
+	call distance_check
+	li a1, 1
+	call test_eq
+
+# TEST: distance_check - too close together
+	la a0, distance_check2name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 1
+	sw t0, 0(a0)
+	li t0, 1
+	sw t0, 4(a0)
+	li t0, 3
+	sw t0, 8(a0)
+	li a1, 12
+	li a2, 1
+	li a3, 3
+	call distance_check
+	li a1, 1
+	call test_eq
+
+# TEST: distance_check - too far apart
+	la a0, distance_check2name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 1
+	sw t0, 0(a0)
+	li t0, 8
+	sw t0, 4(a0)
+	li t0, 10
+	sw t0, 8(a0)
+	li a1, 12
+	li a2, 1
+	li a3, 3
+	call distance_check
+	li a1, 1
+	call test_eq
+
+# TEST: is_report_safe - safe
+	la a0, is_report_safe1name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 1
+	sw t0, 0(a0)
+	li t0, 8
+	sw t0, 4(a0)
+	li t0, 10
+	sw t0, 8(a0)
+	li a1, 12
+	li a2, 1
+	li a3, 3
+	call is_report_safe
+	li a1, 1
+	call test_eq
+
+# TEST: is_report_safe - is_only_inc_or_dec fails
+	la a0, is_report_safe2name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 1
+	sw t0, 0(a0)
+	li t0, 3
+	sw t0, 4(a0)
+	li t0, 2
+	sw t0, 8(a0)
+	li a1, 12
+	li a2, 1
+	li a3, 3
+	call is_report_safe
+	li a1, 1
+	call test_eq
+
+# TEST: is_report_safe - distance_check fails
+	la a0, is_report_safe3name
+	call print
+	li a0, ARRAY_LOC
+	li t0, 1
+	sw t0, 0(a0)
+	li t0, 8
+	sw t0, 4(a0)
+	li t0, 10
+	sw t0, 8(a0)
+	li a1, 12
+	li a2, 1
+	li a3, 3
+	call is_report_safe
+	li a1, 1
+	call test_eq
+
+# TEST: count_reports - counts correct
+	la a0, count_reports1name
+	call print
+	la a0, count_reports1data
+	li a1, 1
+	li a2, 3
+	call count_reports
+	li a1, 2
+	call test_eq
 
 # Print end
 	call end
