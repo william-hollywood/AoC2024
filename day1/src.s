b846c05c915c024255856cf8422a3cfbc9a2049f
William Hollywood <will@oceanplait.com>
2024-12-06 21:49:39 +1300

    Pretty printing

--- day1/src.s
+++ day1/src.s
@@ -6,37 +6,68 @@
 
 .section .data
 newline: .string "\n"
+ok: .string "OK\n"
+phase1text: .string "Phase 1: Load file from stdin: "
+phase2text: .string "Phase 2: Parse dual lists: "
+phase3text: .string "Phase 3: Sort List 1: "
+phase4text: .string "Phase 4: Sort List 2: "
+part1text: .string "Part 1: diff ordered two lists\nAnswer: "
+part2text: .string "Part 2: calulate similarity score\nAnswer: "
 
 # Program main
 .section .text
 	call start
 	addi sp, sp, -16
 
+	la a0, phase1text
+	call print
+
 	li a0, FILE_BUFFER
 	li a1, UART_BASE
 	call gets
 
+	la a0, ok
+	call print
+
+	la a0, phase2text
+	call print
+
 	li a0, FILE_BUFFER
 	li a1, LIST1_POS
 	li a2, LIST2_POS
-
 	call load_dual_list
 	sw a0, 0(sp)
+
+	la a0, ok
+	call print
+
+	la a0, phase3text
+	call print
 
 	li a0, LIST1_POS
 	lw a1, 0(sp)
 	call sort_list
+
+	la a0, ok
+	call print
+
+	la a0, phase4text
+	call print
 
 	li a0, LIST2_POS
 	lw a1, 0(sp)
 	call sort_list
 
+	la a0, ok
+	call print
+
+	la a0, part1text
+	call print
+
 	li a0, LIST1_POS
 	li a1, LIST2_POS
 	lw a2, 0(sp)
 	call diff_lists
-
-bp:
 
 	la a1, RESULT_LOC
 	call itos
@@ -45,6 +76,9 @@ bp:
 	call print
 
 	la a0, newline
+	call print
+
+	la a0, part2text
 	call print
 
 	addi sp, sp, 16
