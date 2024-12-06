f91b5f7b59cb091cab5c37090c173e825fc07871
William Hollywood <will@oceanplait.com>
2024-12-07 11:52:19 +1300

    implement day 2 part 1 main

--- day2/src.s
+++ day2/src.s
@@ -1,17 +1,45 @@
 .equ UART_BASE, 0x10000000
 .equ FILE_BUFFER, 0x84000000
-.equ LIST1, 0x87000000
-.equ LIST2, 0x87000400
+.equ ARR_POS, 0x87000000
+.equ RESULT_LOC, 0x85000000
 
 .section .data
+newline: .string "\n"
+ok: .string "OK\n"
+phase1text: .string "Phase 1: Load file from stdin: "
+part1text: .string "Part 1: count number of valid reports\nAnswer: "
 
 # Program main
 .section .text
 	call start
+	addi sp, sp, -16
+
+	la a0, phase1text
+	call print
+
 	li a0, FILE_BUFFER
 	li a1, UART_BASE
 	call gets
 
-	li a0, FILE_BUFFER
+	la a0, ok
 	call print
+
+	la a0, part1text
+	call print
+
+	li a0, FILE_BUFFER
+	li a1, ARR_POS
+	li a2, 1
+	li a3, 3
+	call count_reports
+
+	la a1, RESULT_LOC
+	call itos
+
+	la a0, RESULT_LOC
+	call print
+
+	la a0, newline
+	call print
+
 	call end
